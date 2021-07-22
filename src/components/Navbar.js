e441045ebd7d269060f16c881bccd5b2fbe877ca
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 16:22:15 +0200

    Starter code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="Navbar ">
+    <nav className="Navbar">
       <div>
-        <NavLink to="/" exact activeClassName="selected">Home</NavLink>
-        <NavLink to="/projects" activeClassName="selected">Projects</NavLink>
+        <NavLink to="/" exact activeClassName="selected">
+          Home
+        </NavLink>
+        <NavLink to="/projects" activeClassName="selected">
+          Projects
+        </NavLink>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,12 @@
-const profileImg = 'https://i.imgur.com/i1gsj0v.png';
-
+const profileImg = "https://i.imgur.com/i1gsj0v.png";
 
 function HomePage() {
   return (
-    <div className={"HomePage " + theme}>
+    <div className="HomePage">
       <h1>Hi! My Name is ...</h1>
       <img src={profileImg} className="profile" alt="profile" />
     </div>
-  )
+  );
 }
 
 export default HomePage;
