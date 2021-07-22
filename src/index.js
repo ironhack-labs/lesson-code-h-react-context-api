cf4250b7be0be76454cc6c36f1c92085c4c0cfee
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 16:27:13 +0200

    Example 3

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { ThemeProvider } from './context/theme.context';    // <== IMPORT
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
