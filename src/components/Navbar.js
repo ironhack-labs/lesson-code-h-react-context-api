816054435842be5da2c080987aeaa40edac4fb73
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 16:16:08 +0200

    Starter code

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <nav className="Navbar ">
+      <div>
+        <NavLink to="/" exact activeClassName="selected">Home</NavLink>
+        <NavLink to="/projects" activeClassName="selected">Projects</NavLink>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProjectCard(props) {
+  return (
+    <div className="ProjectCard">
+      <h3>{props.project.name}</h3>
+      <p>{props.project.technologies}</p>
+    </div>
+  )
+}
+
+export default ProjectCard;
