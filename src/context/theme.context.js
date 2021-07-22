943a3c69400d50d38579450f373ca1582c85734a
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 21:32:21 +0200

    Example 3

--- src/context/theme.context.js
+++ src/context/theme.context.js
@@ -1,16 +1,14 @@
-// src/context/theme.context.js
-
 import React, { createContext } from "react";
 
 const ThemeContext = createContext();
 
-function ThemeProviderWrapper(props) {
+function ThemeProviderWrapper(props) {  // <== CREATE A WRAPPER COMPONENT
 
   return (
-    <ThemeContext.Provider value={"light"}>
+    <ThemeContext.Provider value={"light"}>    {/*  <== SET UP THE PROVIDER INSIDE */}
       {props.children}
     </ThemeContext.Provider>
   )
 }
 
-export { ThemeContext, ThemeProviderWrapper };
+export { ThemeContext, ThemeProviderWrapper };  // <== UPDATE
