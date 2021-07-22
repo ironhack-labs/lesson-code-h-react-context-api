19982b15962ce274932b322b26b22a801b075213
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 17:17:15 +0200

    Example 6

--- src/context/theme.context.js
+++ src/context/theme.context.js
@@ -1,13 +1,20 @@
-// src/context/theme.context.js
-
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";    // <== UPDATE
 
 const ThemeContext = createContext();
 
 function ThemeProvider(props) {
+  const [theme, setTheme] = useState('light');      // <== ADD
+
+  const toggleTheme = () => {                     // <== ADD
+    if (theme === 'light') { 
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    };
+  }  
 
   return (
-    <ThemeContext.Provider value={"light"}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>   {/*  <== UPDATE   */}
       {props.children}
     </ThemeContext.Provider>
   )
