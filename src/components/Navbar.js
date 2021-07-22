c1260e2070c4d26f58eb12994cb1e130774ddcc9
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-22 16:24:01 +0200

    Starter code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,8 @@ function Navbar() {
   return (
     <nav className="Navbar">
       <div>
-        <NavLink to="/" exact activeClassName="selected">
-          Home
-        </NavLink>
-        <NavLink to="/projects" activeClassName="selected">
-          Projects
-        </NavLink>
+        <NavLink to="/" exact activeClassName="selected">Home</NavLink>
+        <NavLink to="/projects" activeClassName="selected">Projects</NavLink>
       </div>
     </nav>
   );
