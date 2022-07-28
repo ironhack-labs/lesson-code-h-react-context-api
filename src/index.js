5466eae629350a26cec89bd81de4646388bd4731
ross-u <cirkovic.uros.srb@gmail.com>
2022-07-28 12:46:55 +0200

    Update to React v18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,15 +7,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ThemeProviderWrapper } from './context/theme.context';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router>
       <ThemeProviderWrapper>
         <App />
       </ThemeProviderWrapper>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 
