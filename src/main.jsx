0610431c0ea7c7b43143262ad1bf0844c051041f
ross-u <cirkovic.uros.srb@gmail.com>
2023-05-22 16:35:46 +0200

    refactor: update starter code to use vite react boilerplate

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { ThemeProviderWrapper } from "./context/theme.context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProviderWrapper>
+        <App />
+      </ThemeProviderWrapper>
+    </Router>
+  </React.StrictMode>
+);
